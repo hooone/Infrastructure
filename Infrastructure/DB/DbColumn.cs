e9a61fd397c35797a35241650c455328f9a7514c
hooone <393412438@qq.com>
2021-04-27 10:21:46 +0800

    添加项目文件。

---
+++ Infrastructure/DB/DbColumn.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.DB
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class DbColumn : Attribute
+    {
+        public DbColumn(DataType type)
+        {
+            Type = type;
+        }
+        public DbColumn(DataType type, int length)
+        {
+            Type = type;
+            Length = length;
+        }
+        public DataType Type { get; set; }
+        public int Length { get; set; }
+    }
+    public enum DataType
+    {
+        /// <summary>
+        /// 定长字符串
+        /// </summary>
+        VARCHAR,
+        /// <summary>
+        /// 可变长度字符串
+        /// </summary>
+        VARCHAR2,
+        /// <summary>
+        /// 整数，默认32位
+        /// </summary>
+        NUMBER,
+        /// <summary>
+        /// 浮点数，默认32位
+        /// </summary>
+        FLOAT,
+        /// <summary>
+        /// 日期时间
+        /// </summary>
+        DATE,
+    }
+}
---
+++ Infrastructure/DB/DbTable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure.DB
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DbTable : Attribute
+    {
+        public string Name { get; set; }
+        public DbTable(string name)
+        {
+            Name = name;
+        }
+    }
+}
