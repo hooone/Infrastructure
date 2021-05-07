ae4cdb4ea3953f393cb75b534f6c753ad38b1292
hooone <393412438@qq.com>
2021-05-07 11:11:05 +0800

    socketserver启动

---
+++ Infrastructure/Log/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Log
+{
+    public interface ILog
+    {
+        void Info(string msg);
+        void Debug(string msg);
+        void Release(string msg);
+    }
+}
---
+++ Infrastructure/Log/NopLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Log
+{
+    public class NopLogger : ILog
+    {
+        public void Debug(string msg)
+        {
+        }
+
+        public void Info(string msg)
+        {
+        }
+
+        public void Release(string msg)
+        {
+        }
+    }
+}
