3fb901eb730356cf933a45120c93106153e1beae
hooone <393412438@qq.com>
2021-05-08 12:26:20 +0800

    目录整理后编译

---
+++ Infrastructure/Log/ILoggerProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Log
+{
+    public interface ILoggerProvider
+    {
+        ILog Logger { get; }
+        void WithLogger(ILog logger);
+    }
+}
