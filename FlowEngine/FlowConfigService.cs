673360e60861e28fa9c563f1a406a502f569974f
hooone <393412438@qq.com>
2021-07-28 09:53:56 +0800

    引入autofac

--- FlowEngine/FlowConfigService.cs
+++ FlowEngine/FlowConfigService.cs
@@ -1,4 +1,5 @@
-﻿using FlowEngine.Model;
+﻿using FlowEngine.DAL;
+using FlowEngine.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,21 +10,31 @@ namespace FlowEngine
 {
     public class FlowConfigService
     {
-        private static FlowConfigService _ins;
-        public static FlowConfigService Instance
+        private readonly NodeDAL nodeDAL = null;
+        private readonly LinkDAL linkDAL = null;
+        private readonly PointDAL pointDAL = null;
+        public FlowConfigService(NodeDAL nodeDAL, LinkDAL linkDAL, PointDAL pointDAL)
         {
-            get
-            {
-                if (_ins != null)
-                    return _ins;
-                _ins = new FlowConfigService();
-                return _ins;
-            }
+            this.nodeDAL = nodeDAL;
+            this.linkDAL = linkDAL;
+            this.pointDAL = pointDAL;
         }
         public FlowConfig GetFlowConfig()
         {
             FlowConfig rst = new FlowConfig();
             rst.Nodes = new List<NodeProperty>();
+            var nodes = nodeDAL.read();
+            foreach (var nd in nodes)
+            {
+                NodeProperty node = new NodeProperty();
+                node.Id = nd.ID;
+                node.Text = nd.TEXT;
+                node.X = nd.X;
+                node.Y = nd.Y;
+                node.LinkIn = new Dictionary<string, int>();
+                node.LinkOut = new Dictionary<string, int>();
+                rst.Nodes.Add(node);
+            }
             return rst;
         }
     }
---
+++ FlowEngine/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Infrastructure.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowEngine
+{
+    public class Launcher
+    {
+        public IContainer Container = null;
+        public void InitAutoFac()
+        {
+            var builder = new ContainerBuilder();
+            // DB
+            builder.RegisterType<OracleHelper>().As<SqlHelper>().SingleInstance();
+            // DAL
+            builder.RegisterType<DAL.LinkDAL>().SingleInstance();
+            builder.RegisterType<DAL.NodeDAL>().SingleInstance();
+            builder.RegisterType<DAL.PointDAL>().SingleInstance();
+            // Service
+            builder.RegisterType<FlowConfigService>().SingleInstance();
+            Container = builder.Build();
+        }
+    }
+}
